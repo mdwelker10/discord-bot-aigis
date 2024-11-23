0428deef28d62c60759b918aeff45e117172c1e4
trashpanda70 <mattw8824@gmail.com>
2024-11-22 23:33:10 -0500

    delete commands fix

--- scripts/delete-commands.js
+++ scripts/delete-commands.js
@@ -7,7 +7,7 @@ const rest = new REST().setToken(token);
 
 if (process.env.DEV == 1) {
   rest.put(Routes.applicationGuildCommands(clientId, process.env.TEST_GUILD, { body: [] })).then(() => console.log('deleted from guild1')).catch(console.error);
-  rest.put(Routes.applicationGuildCommands(clientId, process.env.TEST_GUILD2, { body: [] })).then(() => console.log('deleted from guild2')).catch(console.error);
+  rest.put(Routes.applicationGuildCommands(clientId, process.env.TEST_GUILD_2, { body: [] })).then(() => console.log('deleted from guild2')).catch(console.error);
   //rest.put(Routes.applicationGuildCommands(clientId, process.env.TEST_GUILD, "CommandID")).then(() => console.log('deleted')).catch(console.error);
   //rest.put(Routes.applicationGuildCommands(clientId, process.env.TEST_GUILD2, "CommandID")).then(() => console.log('deleted')).catch(console.error);
 } else {
