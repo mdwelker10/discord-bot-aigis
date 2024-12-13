99ba78d517515b405abcf268693350d2bd6e66b6
trashpanda70 <mattw8824@gmail.com>
2024-12-13 16:05:47 -0500

    testing webhook

--- ai/cudacheck.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print(torch.cuda.is_available())  # Should return True if CUDA is available
-print(torch.cuda.device_count())  # Number of GPUs available
-
---
+++ commands/admin/command.js
@@ -0,0 +1,5 @@
+/*
+/command enable X
+/command disable X
+X = command name, get from command files like in index.js or manual list/array. Use Autocomplete
+*/
