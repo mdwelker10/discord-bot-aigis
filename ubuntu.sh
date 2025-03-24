7967e6926b9172743cdb87ff80c29befe9e70202
trashpanda70 <mattw8824@gmail.com>
2025-03-24 19:36:06 -0400

    testing ubuntu hook 2

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Script to deploy to Ubuntu (or rather just listed steps)
 sudo apt-get update
 sudo apt-get install -y gnupg curl git ttf-mscorefonts-installer fontconfig jq nano redis-server
